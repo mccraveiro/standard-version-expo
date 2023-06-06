9e79a72ee3cc8acfa46f7a63d8f7085b089110b3
Mateus Craveiro <mccraveiro@gmail.com>
2023-06-06 08:25:12 -0300

    fix: build

---
+++ @types/xcode.d.ts
@@ -0,0 +1,9 @@
+declare module 'xcode' {
+  export type XcodeProject = any;
+  export type PBXProject = any;
+  export type PBXNativeTarget = any;
+  export type XCConfigurationList = any;
+  export type XCBuildConfiguration = any;
+  export type PBXGroup = any;
+  export type PBXFile = any;
+}
